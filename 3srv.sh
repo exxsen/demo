df0b08da120774e630171488acf08f38a2f3257d
-exsen <102148118+exxsen@users.noreply.github.com>
2026-06-17 02:51:04 +0300

    Create 3srv.sh

---
+++ 3srv.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+set -o history
+
+run() {
+    history -s "$*"
+    eval "$*"
+}
+
+run "useradd sshuser -u 1010"
+run "echo 'sshuser:P@ssw0rd' | chpasswd"
+run "usermod -aG wheel sshuser"
+history -s "nano /etc/sudoers"
+echo "sshuser ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+
+history -w
