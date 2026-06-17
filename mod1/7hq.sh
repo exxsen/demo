30c98fd28b6d95576b309c74627085d39dab831e
-exsen <102148118+exxsen@users.noreply.github.com>
2026-06-17 03:03:49 +0300

    Create 7hq.sh

---
+++ mod1/7hq.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+set -o history
+
+run() {
+    history -s "$*"
+    eval "$*"
+}
+
+run "apt-get install -y frr"
+
+history -s "nano /etc/frr/daemons"
+sed -i 's/^ospfd=.*/ospfd=yes/' /etc/frr/daemons
+
+run "systemctl enable --now frr"
+
+
+run "vtysh -c 'configure terminal' -c 'router ospf' -c 'router-id 1.1.1.1' -c 'network 192.168.5.0/28 area 0' -c 'network 192.168.6.0/26 area 0' -c 'network 10.0.1.0/30 area 0' -c 'exit' -c 'interface gre1' -c 'ip ospf network point-to-point' -c 'ip ospf authentication message-digest' -c 'ip ospf message-digest-key 1 md5 P@ssw0rd' -c 'end' -c 'write memory'"
+
+history -w
