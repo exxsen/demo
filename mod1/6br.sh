01a20a7ff178b93fc278691b141d2c33bad1192d
-exsen <102148118+exxsen@users.noreply.github.com>
2026-06-17 03:26:47 +0300

    Update 6br.sh

--- mod1/6br.sh
+++ mod1/6br.sh
@@ -11,14 +11,7 @@ run() {
 run "mkdir -p /etc/net/ifaces/gre1"
 
 history -s "nano /etc/net/ifaces/gre1/options"
-cat > /etc/net/ifaces/gre1/options <<'EOF'
-TYPE=iptun
-TUNTYPE=gre
-TUNLOCAL=172.16.5.2
-TUNREMOTE=172.16.4.2
-TUNOPTIONS='ttl 64'
-DISABLED=no
-EOF
+echo -e "TYPE=iptun\nTUNTYPE=gre\nTUNLOCAL=172.16.5.2\nTUNREMOTE=172.16.4.2\nTUNOPTIONS='ttl 64'\nDISABLED=no" > /etc/net/ifaces/gre1/options
 
 history -s "nano /etc/net/ifaces/gre1/ipv4address"
 echo "10.0.1.2/30 peer 10.0.1.1" > /etc/net/ifaces/gre1/ipv4address
