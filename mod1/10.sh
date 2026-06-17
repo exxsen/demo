236123ae8b4fb081f5a97089afa1aa283d108c54
-exsen <102148118+exxsen@users.noreply.github.com>
2026-06-17 03:28:44 +0300

    Update 10.sh

--- mod1/10.sh
+++ mod1/10.sh
@@ -24,54 +24,17 @@ options {
 };
 EOF
 
+history -s "nano /etc/bind/options.conf"
+echo -e 'options {\n    directory "/var/lib/bind";\n    listen-on { any; };\n    forwarders { 77.88.8.8; };\n    allow-query { any; };\n    recursion yes;\n};' > /etc/bind/options.conf
+
 history -s "nano /etc/bind/local.conf"
-cat > /etc/bind/local.conf <<'EOF'
-zone "au-team.irpo" {
-    type master;
-    file "/etc/bind/au-team.irpo.db";
-};
-zone "168.192.in-addr.arpa" {
-    type master;
-    file "/etc/bind/192.168.rev";
-};
-EOF
+echo -e 'zone "au-team.irpo" {\n    type master;\n    file "/etc/bind/au-team.irpo.db";\n};\nzone "168.192.in-addr.arpa" {\n    type master;\n    file "/etc/bind/192.168.rev";\n};' > /etc/bind/local.conf
 
 history -s "nano /etc/bind/au-team.irpo.db"
-cat > /etc/bind/au-team.irpo.db <<'EOF'
-$TTL    1D
-@       IN      SOA     au-team.irpo. root.au-team.irpo. (
-                        2026012400      ; serial
-                        12H             ; refresh
-                        1H              ; retry
-                        1W              ; expire
-                        1H              ; ncache
-                        )
-        IN      NS      hq-srv.au-team.irpo.
-hq-srv  IN      A       192.168.6.2
-hq-rtr  IN      A       192.168.6.1
-br-rtr  IN      A       192.168.7.1
-hq-cli  IN      A       192.168.5.3
-br-srv  IN      A       192.168.7.2
-moodle  IN      A       172.16.4.1
-wiki    IN      A       172.16.5.1
-EOF
+echo -e '$TTL    1D\n@       IN      SOA     au-team.irpo. root.au-team.irpo. (\n                        2026012400      ; serial\n                        12H             ; refresh\n                        1H              ; retry\n                        1W              ; expire\n                        1H              ; ncache\n                        )\n        IN      NS      hq-srv.au-team.irpo.\nhq-srv  IN      A       192.168.6.2\nhq-rtr  IN      A       192.168.6.1\nbr-rtr  IN      A       192.168.7.1\nhq-cli  IN      A       192.168.5.3\nbr-srv  IN      A       192.168.7.2\nmoodle  IN      A       172.16.4.1\nwiki    IN      A       172.16.5.1' > /etc/bind/au-team.irpo.db
 
 history -s "nano /etc/bind/192.168.rev"
-cat > /etc/bind/192.168.rev <<'EOF'
-$TTL    1D
-@       IN      SOA     au-team.irpo. root.au-team.irpo. (
-                        2026012400      ; serial
-                        12H             ; refresh
-                        1H              ; retry
-                        1W              ; expire
-                        1H              ; ncache
-                        )
-        IN      NS      hq-srv.au-team.irpo.
-2.6     IN  PTR hq-srv.au-team.irpo.
-1.6     IN  PTR hq-rtr.au-team.irpo.
-3.5     IN  PTR hq-cli.au-team.irpo.
-EOF
-
+echo -e '$TTL    1D\n@       IN      SOA     au-team.irpo. root.au-team.irpo. (\n                        2026012400      ; serial\n                        12H             ; refresh\n                        1H              ; retry\n                        1W              ; expire\n                        1H              ; ncache\n                        )\n        IN      NS      hq-srv.au-team.irpo.\n2.6     IN  PTR hq-srv.au-team.irpo.\n1.6     IN  PTR hq-rtr.au-team.irpo.\n3.5     IN  PTR hq-cli.au-team.irpo.' > /etc/bind/192.168.rev
 
 run "named-checkconf /etc/bind/options.conf"
 run "named-checkconf /etc/bind/local.conf"
