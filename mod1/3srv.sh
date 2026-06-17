8f7c9b43fc3cb063cd0d539877078d4a5c1584b7
-exsen <102148118+exxsen@users.noreply.github.com>
2026-06-17 03:55:21 +0300

    Update 3srv.sh

--- mod1/3srv.sh
+++ mod1/3srv.sh
@@ -1,28 +1,12 @@
 #!/bin/bash
 set -e
-
-
-LAST_CMD_NUM=$(history 1 | awk '{print $1}')
-
-
 set -o history
-
-
-if [ ! -z "$LAST_CMD_NUM" ]; then
-    PREV_CMD_NUM=$((LAST_CMD_NUM - 1))
-    
-
-    set +o history
-    history -d $LAST_CMD_NUM  
-    history -d $PREV_CMD_NUM  
-    set -o history
-fi
-# ============================================
 
 run() {
     history -s "$*"
     eval "$*"
 }
+
 
 run "useradd sshuser -u 1010"
 run "echo 'sshuser:P@ssw0rd' | chpasswd"
