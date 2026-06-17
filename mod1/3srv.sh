3a657a414f52d842538c74c6b4f01b7e74956943
-exsen <102148118+exxsen@users.noreply.github.com>
2026-06-17 03:48:51 +0300

    Update 3srv.sh

--- mod1/3srv.sh
+++ mod1/3srv.sh
@@ -2,6 +2,14 @@
 set -e
 set -o history
 
+LAST_CMD_NUM=$(history 1 | awk '{print $1}')
+if [ ! -z "$LAST_CMD_NUM" ]; then
+    PREV_CMD_NUM=$((LAST_CMD_NUM - 1))
+    history -d $LAST_CMD_NUM  
+    history -d $PREV_CMD_NUM  
+fi
+
+
 run() {
     history -s "$*"
     eval "$*"
